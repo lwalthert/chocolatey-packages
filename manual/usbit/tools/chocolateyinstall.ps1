3a5faffda05b471be0f1cdcab283989bfcebeda3
lwalthert <lwalthert@users.noreply.github.com>
2016-04-14 12:59:58 +0200

    Added USB Image Tool

---
+++ manual/usbit/tools/chocolateyinstall.ps1
@@ -0,0 +1,10 @@
+﻿
+$ErrorActionPreference = 'Stop';
+
+$packageName= 'usbit'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$target     = Join-Path $toolsDir 'USB Image Tool.exe'
+$url        = 'http://www.alexpage.de/download/usbit/usbit.zip'
+
+Install-ChocolateyZipPackage $packageName $url $toolsDir
+Install-ChocolateyDesktopLink -TargetFilePath $target
---
+++ manual/usbit/tools/chocolateyuninstall.ps1
@@ -0,0 +1,8 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$packageName = 'usbit'
+$desktop = $([System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::DesktopDirectory))
+$shortcut_to_remove = 'USB Image Tool.exe.lnk'
+
+Remove-Item "$desktop\$shortcut_to_remove" -ErrorAction Continue
+Uninstall-ChocolateyZipPackage $packageName
